b5990e079bcf4eec2412e445fe07ef2c3af3f333
Faizan Durrani <faizandurrani324@gmail.com>
2013-06-26 14:11:12 +0500

    Login function update
    updated to use new db format

--- auth/includes/connect.php
+++ auth/includes/connect.php
@@ -38,7 +38,7 @@ function res_det($table_name)
 function loginuser($user, $password, $type)
 {
     if ($type == "student") {
-        $sql = mysql_query("SELECT * FROM stu_login WHERE `epnum` = '$user' AND `password` = '$password'") or die (mysql_error());
+        $sql = mysql_query("SELECT * FROM student WHERE `seatno` = '$user' AND `password` = '$password'") or die (mysql_error());
         $sql_results = mysql_fetch_array($sql);
         $user_exists = mysql_num_rows($sql);
         if ($user_exists == 1) {
@@ -52,7 +52,7 @@ function loginuser($user, $password, $type)
         $loginerror = "Incorrect username or password.";
     }
     if ($type == "admin") {
-        $sql = mysql_query("SELECT * FROM admin_login WHERE `username` = '$user' AND `password` = '$password'") or die (mysql_error());
+        $sql = mysql_query("SELECT * FROM administrator WHERE `adminid` = '$user' AND `password` = '$password'") or die (mysql_error());
         $sql_results = mysql_fetch_array($sql);
         $user_exists = mysql_num_rows($sql);
         if ($user_exists == 1) {
